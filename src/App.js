fdee39f4cf14ca44b64570e8f7a660cb9b84867e
Richard Hua <hua.richard0@gmail.com>
2024-01-11 12:27:31 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import './App.css';
+import "./App.css";
 import { useEffect, useState } from "react";
-import socketIO from 'socket.io-client';
-const socket = socketIO.connect("http://localhost:3500")
+import socketIO from "socket.io-client";
+const socket = socketIO.connect("http://localhost:3500");
 function App() {
-  const [messages, updateMessages] = useState([])
+  const [messages, updateMessages] = useState([]);
+  const [messageList, updateMessageList] = useState([]);
+
   function message() {
-    socket.emit("message", socket.id.substring(0,5) + " said hi")
+    socket.emit("message", socket.id.substring(0, 5) + " said hi");
   }
 
-  useEffect (() => {
+  useEffect(() => {
     socket.on("message", (data) => {
-      updateMessages(messages + " " + data)
-    })
-  })
+      updateMessages(messages + " " + data);
+      updateMessageList([...messageList, messages + " " + data]);
+    });
+  });
   return (
     <div>
-      <input></input><button onClick={() => {message()}}>Send</button>
-      <div>{messages}</div>
+      <input></input>
+      <button
+        onClick={() => {
+          message();
+        }}
+      >
+        Send
+      </button>
+      <div className="container">
+        {messageList.map((m) => <div className="container">{m}</div>)}
+      </div>
     </div>
-    );
+  );
 }
 
 export default App;
