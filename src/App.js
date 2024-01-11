dbdb25d94b36870e6645125fb3a0a42342f595b8
Richard Hua <hua.richard0@gmail.com>
2024-01-11 16:32:28 -0500

    Fixed

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { useEffect, useState } from "react";
 import socketIO from "socket.io-client";
 const socket = socketIO.connect("http://localhost:3500");
 function App() {
-  const [messages, updateMessages] = useState([]);
   const [messageList, updateMessageList] = useState([]);
 
   function message() {
     socket.emit("message", socket.id.substring(0, 5) + " said hi");
+    console.log(messageList)
   }
 
   useEffect(() => {
     socket.on("message", (data) => {
-      updateMessages(messages + " " + data);
-      updateMessageList([...messageList, messages + " " + data]);
+      updateMessageList([...messageList," " + data]);
     });
   });
+
   return (
     <div>
       <input></input>
@@ -26,9 +26,7 @@ function App() {
       >
         Send
       </button>
-      <div className="container">
-        {messageList.map((m) => <div className="container">{m}</div>)}
-      </div>
+      {messageList.map((m) => (<div className="container">{m}</div>))}
     </div>
   );
 }
