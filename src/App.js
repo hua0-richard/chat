cc08b21b091cc2c560b25e25e170bc1cee949dfa
Richard Hua <hua.richard0@gmail.com>
2024-01-13 01:28:56 -0500

    Scroll Into View

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import TextBubble from "./components/TextBubble";
 import { FaPaperPlane } from "react-icons/fa";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import socketIO from "socket.io-client";
 // change to node env
 const socket = socketIO.connect("http://localhost:3500");
 function App() {
   const [messageList, updateMessageList] = useState([]);
   const [text, updateText] = useState("");
+  const targetElementRef = useRef(null);
+
+  // Function to scroll to the target element
+  const scrollToTarget = () => {
+    targetElementRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
   function handleUpdateText(event) {
     updateText(event.target.value);
@@ -24,38 +30,40 @@ function App() {
   useEffect(() => {
     socket.on("message", (mes) => {
       updateMessageList([...messageList, mes]);
+      scrollToTarget();
     });
   });
 
   return (
-      <div>
-        <div className="Header">
-          <h1 style={{width: "170px"}}>Chat Room</h1>
-        </div>
-        <div className="message-container">
-          {messageList.map((m) => (
-            <TextBubble data={m} user={socket.id}></TextBubble>
-          ))}
-        </div>
+    <div>
+      <div className="Header">
+        <h1 style={{ width: "170px" }}>Chat Room</h1>
+      </div>
+      <div className="message-container">
+        {messageList.map((m) => (
+          <TextBubble data={m} user={socket.id}></TextBubble>
+        ))}
+      </div>
+      <div ref={targetElementRef}></div>
 
-        <div className="chat">
-          <div className="chat-container">
-            <input
-              placeholder="Type Here..."
-              className="chatbox"
-              value={text}
-              onChange={handleUpdateText}
-            ></input>
-            <div className="send-container">
-              <FaPaperPlane
-                onClick={() => {
-                  message();
-                }}
-              />
-            </div>
+      <div className="chat">
+        <div className="chat-container">
+          <input
+            placeholder="Type Here..."
+            className="chatbox"
+            value={text}
+            onChange={handleUpdateText}
+          ></input>
+          <div className="send-container">
+            <FaPaperPlane
+              onClick={() => {
+                message();
+              }}
+            />
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
